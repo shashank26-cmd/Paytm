3e14dcda0f22e651d2889a87ce43c7601d7ffd05
unknown <shashankbud80@gmail.com>
2024-12-11 23:48:32 +0530

    first commit

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -7,6 +7,7 @@ export default function Page(): JSX.Element {
   return (
    <div>
       <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+      <div>hello hey guys </div>
    </div>
   );
 }
